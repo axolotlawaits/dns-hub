7b40d309b1c25dc28e4435484e63826143f91295
axolotlawaits <superintendya@yandex.ru>
2025-04-10 17:39:26 +0700

    Add theme logic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import { MantineProvider } from '@mantine/core'
 import { UserContextProvider } from './contexts/UserContext.jsx'
+import { ThemeContextProvider } from './contexts/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserContextProvider>
-      <MantineProvider>
-        <App />
-      </MantineProvider>
+      <ThemeContextProvider>
+        <MantineProvider>
+          <App />
+        </MantineProvider>
+      </ThemeContextProvider>
     </UserContextProvider>
   </StrictMode>,
 )
